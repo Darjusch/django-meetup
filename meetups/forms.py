6bf37f71ba05ea057989e31b79927b7ec397b58a
Darjusch <Darjusch.schrand@code.berlin>
2021-08-30 13:38:41 +0200

    clean up

--- meetups/forms.py
+++ meetups/forms.py
@@ -1,8 +1,5 @@
 from django import forms
-from .models import Participant
 
 
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model = Participant # takes the fields from the Participant model 
-        fields = ['email'] # restricts which fields should be taken from the model
+class RegistrationForm(forms.Form):
+    email = forms.EmailField(label='Your email')
